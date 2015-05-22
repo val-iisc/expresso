99994124a6ce8d0a668a07a52dab96665d5b107b
Ravi Kiran <ravika@gmail.com>
2015-05-22 12:51:23 +0530

    Fixed proxy check test syntax error in install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ sudo apt-get install pyqt4-dev-tools
 sudo apt-get install libqt4-dev
 sudo apt-get install python-h5py
 
-if [[ -n "$HTTP_PROXY" ]]; then
+if [ -n "$HTTP_PROXY" ]; then
 echo Proxy is $HTTP_PROXY
 sudo pip install --proxy $HTTP_PROXY pyqtgraph
 sudo pip install --proxy $HTTP_PROXY qtutils 
 else
+echo No proxy
 sudo pip install pyqtgraph
 sudo pip install qtutils 
 fi
